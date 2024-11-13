743ec29e37eadf27a2b2412da966930e4171205d
Szilveszter Marinka <szilvesztermarinka4@gmail.com>
2024-11-13 11:53:08 +0100

    App route, Post model

---
+++ backend/controllers/app.controller.js
@@ -0,0 +1,39 @@
+import { CookingPot } from "lucide-react";
+import { prisma } from "../config/prismaclient.js";
+
+export const getAllAd = async (req, res) => {
+    try {
+        const posts = await prisma.post.findMany();
+        
+        res.status(200).json({success: true, posts})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const addPost = async (req, res) => {
+    try {
+        const {property, type, price, description, deposit, bedroom, livingroom, images, balcony, ownerId} = req.body;
+
+        console.log(req.body)
+
+        const post = await prisma.post.create({
+            data: {
+                property,
+                type, 
+                price,
+                description,
+                deposit,
+                bedroom,
+                livingroom,
+                images,
+                balcony,
+                ownerId
+            }
+        })
+
+        res.status(201).json({success: true, post})
+    } catch (error) {
+        console.log(error)
+    }
+}
--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,21 @@
 import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
-import cors from "cors"
-
-import { connectDB } from "./db/connectDB.js";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
+import appRoutes from "./routes/app.route.js"
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors({ origin:"http://localhost:5173", credentials: true}));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json()); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse cookies
 
 app.use("/api/auth", authRoutes);
-
+app.use("/api/app", appRoutes);
 
 app.listen(PORT, () => {
-    connectDB();
-    console.log("Server is running on port: ", PORT);
+  console.log("Server is running on port: ", PORT);
 });
